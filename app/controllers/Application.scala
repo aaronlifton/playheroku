fa2e21173ecb199f5ee8f3fe57eb316ecb794b2c
Aaron Lifton <theflubba@gmail.com>
2013-03-28 21:17:08 -0400

    commit

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -25,6 +25,20 @@ import play.api.libs.json.util._
 import play.api.libs.json.Reads._
 import play.api.data.validation.ValidationError
 
+import play.api.Play.current
+import akka.util.Timeout
+import scala.concurrent._ //{ Await, Future }
+import play.api.libs.concurrent.Execution.Implicits._
+import scala.concurrent.duration._
+
+import java.util.Date
+import java.net.URLEncoder
+import play.api.libs.ws
+import play.api.libs.ws.WS
+
+import play.api.libs.json
+import play.api.libs.json.{JsObject, JsValue}
+
 object JacksonWrapper {
   val mapper = new ObjectMapper()
   mapper.registerModule(DefaultScalaModule)
@@ -54,31 +68,33 @@ object JacksonWrapper {
 }
 
 class Pusher {
-  val appId = Play.configuration.getProperty("pusher.appId")
-  val key = Play.configuration.getProperty("pusher.key")
-  val secret = Play.configuration.getProperty("pusher.secret")
+  val appId = Play.configuration.getString("pusher.appId")
+  val key = Play.configuration.getString("pusher.key")
+  val secret = Play.configuration.getString("pusher.secret")
   
   import java.security.MessageDigest
   import java.math.BigInteger
   import javax.crypto.Mac
   import javax.crypto.spec.SecretKeySpec
   
-  def trigger(channel:String, event:String, message:String):HttpResponse = {
+  def trigger(channel:String, event:String, message:String): ws.Response = {
     val domain = "api.pusherapp.com"
     val url = "/apps/"+appId+"/channels/"+channel+"/events";
     val body = message
-    
+    // val body = JsObject(Seq("test" -> message)) Seq([String, JsValue])
     val params = List( 
       ("auth_key", key),
       ("auth_timestamp", (new Date().getTime()/1000) toInt ),
       ("auth_version", "1.0"),
       ("name", event),
       ("body_md5", md5(body))
-    ).sort( (a,b) => a._1 < b._1 ).map( o => o._1+"="+WS.encode(o._2.toString) ).mkString("&");
+    ).sortWith((a,b) => a._1 < b._1 ).map( o => o._1+"="+URLEncoder.encode(o._2.toString)).mkString("&");
     
-    val signature = sha256( List("POST", url, params).mkString("\n"), secret ); 
-     
-    WS.url("http://"+domain+url+"?"+params+"&auth_signature="+WS.encode(signature)).body(body).post()
+    val signature = sha256(List("POST", url, params).mkString("\n"), secret.get); 
+    val signatureEncoded = URLEncoder.encode(signature, "UTF-8");
+	implicit val timeout = Timeout(5 seconds)
+	val f = WS.url("http://"+domain+url+"?"+params+"&auth_signature="+signatureEncoded).post(body)
+	Await.result(f,timeout.duration)
   }
   
   def byteArrayToString(data: Array[Byte]) = {
@@ -89,19 +105,23 @@ class Pusher {
   
   def sha256(s: String, secret: String):String = {
     val mac = Mac.getInstance("HmacSHA256");
-    mac.init(new SecretKeySpec( secret.getBytes(), "HmacSHA256"));
+    mac.init(new SecretKeySpec(secret.getBytes(), "HmacSHA256"));
     val digest = mac.doFinal(s.getBytes());
     String.format("%0" + (digest.length << 1) + "x", new BigInteger(1, digest));
   }
 }
 
 object WebSockets extends Pusher {
-  val channel = Play.configuration.getProperty("websockets.channel")
-  def trigger(event:String, message:String):HttpResponse = trigger(channel, event, message)
+  val channel = "new-messages-scala" // Play.configuration.getString("websockets.channel")
+  def trigger(event:String, message:String): ws.Response = trigger(channel, event, message)
 }
  
 object Test {
   def test = WebSockets.trigger("hello", "{ \"message\": \"test\" }")
+}
+
+object MessagePusher {
+  def newMessage(json: String) = WebSockets.trigger("new-message", json)
 }
 
 object Application extends Controller {
@@ -145,7 +165,7 @@ object Application extends Controller {
   def getThreads = Action {
      val threads = Thread.findAll()
 	 val json = JacksonWrapper.serialize(threads)
-	 Test.test()
+	 // Test.test
      Ok(json).as(JSON)
   }
   
@@ -162,6 +182,7 @@ object Application extends Controller {
 			case (body) =>
 				val messageId = Message.createAndReturnId(Message(NotAssigned, threadId, body))
 			    val json = JacksonWrapper.serialize(messageId)
+                MessagePusher.newMessage(json)
 				Ok(json).as(JSON)
 		  		// Redirect(routes.Application.index())
 		}
